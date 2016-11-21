42ba3b3d9060ee42c728c2aa4fad4a8392b35f41
tsqn <tushqan1@mail.ru>
2016-11-21 17:08:39 +0700

    +sliser

--- TimeMachine/main.cpp
+++ TimeMachine/main.cpp
@@ -3,6 +3,7 @@
 #include "player.h"
 #include "insert.h"
 #include "select.h"
+#include "slicer.h"
 #include <iostream>
 #include <QApplication>
 #include <QObject>
@@ -10,33 +11,49 @@
 int main(int argc, char *argv[])
 {
     {
-        QDateTime firstDateTime(QDate(2000,01,01),QTime(00,00,00));
-        quint32 time = firstDateTime.toTime_t();
-        QString iFileName = "slice_0-5.ts";
-        //QDir iFilePath = QFileInfo("slice_0-5.ts").dir();
-        QString iSourceName = "CAM01";
-        QString iSourceAdress = "1.1.1.1";
-        qint32  iOffset = 0;
-        for(int i = 0; i < 366; ++i)
+        Slicer slicer;
+        const char* input ="1080p25.ts";
+        const char* output1 ="slice1.ts";
+        const char* output2 ="slice2.ts";
+        //slicer.convertToTS(input,output);
+        //input = output;
+        //output = "slice.ts";
+        uint start = 0;
+        uint stop = start + 29;
+        slicer.makeSlice(input,start,stop,output1);
         {
-            iOffset++;
-            Insert::insert(++time,iFileName,iSourceName,iSourceAdress,"home/",iOffset);
+            QDateTime DateTime(QDate(2002,01,01),QTime(00,00,01));
+            quint32 time = DateTime.toTime_t();
+            QString iFileName = output1;
+            //QString iFilePath = QFileInfo("slice_0-5.ts").dir();
+            QString iSourceName = "CAM01";
+            QString iSourceAdress = "1.1.1.1";
+            qint32  iOffset = 0;
+            for(int i = 0; i < stop; ++i)
+            {
+                iOffset++;
+                Insert::insert(++time,iFileName,iSourceName,iSourceAdress,"home/",iOffset);
+            }
         }
-    }
-    {
-        QDateTime firstDateTime(QDate(2000,01,01),QTime(00,06,07));
-        quint32 time = firstDateTime.toTime_t();
-        QString iFileName = "slice_5-10.ts";
-        //QDir iFilePath = QFileInfo("slice_5-10.ts").dir();
-        QString iSourceName = "CAM01";
-        QString iSourceAdress = "1.1.1.1";
-        qint32  iOffset = 0;
-        for(int i = 0; i < 306; ++i)
+        start = start+stop;
+        stop = start + 29;
         {
-            iOffset++;
-            Insert::insert(++time,iFileName,iSourceName,iSourceAdress,"home/",iOffset);
+            QDateTime DateTime = DateTime.addSecs(start-29);
+            quint32 time = DateTime.toTime_t();
+            QString iFileName = output2;
+            //QString iFilePath = QFileInfo("slice_5-10.ts").dir();
+            QString iSourceName = "CAM01";
+            QString iSourceAdress = "1.1.1.1";
+            qint32  iOffset = 0;
+            for(int i = 0; i < stop; ++i)
+            {
+                iOffset++;
+                Insert::insert(++time,iFileName,iSourceName,iSourceAdress,"home/",iOffset);
+            }
         }
+        slicer.makeSlice(input,start,stop,output2);
     }
+
     /*
     qint32 offset = Select::selectOffset(time-66);
     QString path = Select::selectPath(time-66);
