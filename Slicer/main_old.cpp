dc0de9e6118a6c7db9ca85576c882a356536bd6c
Riottomsk <bizin_kir@sibmail.com>
2016-12-01 04:28:02 +0700

    Added record from rtsp

--- Slicer/main_old.cpp
+++ Slicer/main_old.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     Slicer slicer;    
-    const char* input ="http://ar.solumedia.com.ar:1935/cool/hd/playlist.m3u8"; //"D:\\Work\\TD\\test\\Projects\\TestLibVLC\\video\\output4.ts";
+    const char* input ="rtsp://ewns-hls-b-stream.hexaglobe.net/rtpeuronewslive/en_vidan750_rtp.sdp"; //"http://ar.solumedia.com.ar:1935/cool/hd/playlist.m3u8"; //"D:\\Work\\TD\\test\\Projects\\TestLibVLC\\video\\output4.ts";
     const char* output ="D:\\Work\\TD\\test\\Projects\\TestLibVLC\\video\\slices\\slice_5-10.ts";
     const char* outputFromStream = "D:\\Work\\TD\\test\\Projects\\video\\outputFromStream.ts";
     //slicer.convertToTS(input,output);
@@ -19,7 +19,12 @@ int main(int argc, char *argv[])
     //slicer.convertToTsFromStream(input,outputFromStream); // there is no stop yet, so turn it off by yourself
     //slicer.makeSliceFromStream();
     int numberOfSlices = 3;
-    slicer.makeMultipleSlices(input, numberOfSlices);
+    const char* output_location = "D:\\example";
+    slicer.makeMultipleSlices(input, output_location, numberOfSlices);
+    //qDebug()<<slicer.getDuration("D:\\Work\\TD\\test\\Projects\\video\\outputFromStream_1.ts");
+    //slicer.testFunc();
+    //slicer.convertToTS("D:\\example\\outputFromStream.ts","D:\\example\\outputFromStream.ts");
+    //slicer.makeSliceFromStreamSmem();
 
 
     qDebug()<<"End of main";
--- Slicer/slicer.cpp
+++ Slicer/slicer.cpp
@@ -8,11 +8,10 @@
 #include <chrono>
 #include <thread>
 
-
 void Slicer::convertToTS(const char* input, const char* output)
 {
-    const char* sout="--sout=#standard{access=file,mux=ts,dst=\"";
-    const char* ending="\"}";
+    const char* sout="--sout=#standard{access=file,mux=ts,dst=";
+    const char* ending="}";
     char *param;
     param = new char[strlen(sout)+strlen(output)+strlen(ending)];
     strcat(strcpy(param,sout),output);
@@ -21,20 +20,28 @@ void Slicer::convertToTS(const char* input, const char* output)
     qDebug()<<param;
     const char * const vlc_args[] =
     {
-        "--verbose=2",
+        //"--verbose=2",
         param,
         //"--sout=#standard{access=file,mux=ts,dst=\"D:\\Work\\TD\\test\\Projects\\build-Slicer-Desktop_Qt_5_7_0_MinGW_32bit-Debug\\debug\\video\\output.ts\"}",
         //"--sout-transcode-deinterlace",
         //"--packetizer-mpegvideo-sync-iframe",
         //"--sout-ts-use-key-frames",
         //"--sout-ts-shaping=50",
+        "vlc://quit",
     };
 
+    libvlc_instance_t *inst_tmp;
+    libvlc_media_player_t *mp_tmp;
+    libvlc_media_t *m_tmp;
 
-    inst=libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
-    m = libvlc_media_new_path (inst, input);
-    mp = libvlc_media_player_new_from_media (m);
-    libvlc_media_player_play(mp);
+    inst_tmp=libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
+    m_tmp = libvlc_media_new_path(inst_tmp, input);
+    mp_tmp = libvlc_media_player_new_from_media(m_tmp);
+    libvlc_media_player_play(mp_tmp);
+    qDebug()<<"Hi1";
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000)); //TODO: Event catcher! or there is waste of time
+    qDebug()<<"Hi2";
+    libvlc_media_player_stop(mp_tmp);
 }
 
 void Slicer::splitTS(const char* input)
@@ -136,13 +143,19 @@ int Slicer::getDuration(const char* input)
     const char * const vlc_args[] =
     {
         //"--verbose=2",
+        //"-I","dummy",
         "vlc://quit",
     };
     inst=libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
     m = libvlc_media_new_path (inst, input);
     mp = libvlc_media_player_new_from_media (m);
     libvlc_media_parse(m);
+    //libvlc_media_player_play(mp);
+    //libvlc_media_player_stop(mp);
+
+    //libvlc_video_get_
     return libvlc_media_get_duration(m);
+    //return libvlc_media_player_get_length(mp);
 }
 
 void Slicer::convertToTsFromStream(const char* input, const char* output)
@@ -156,7 +169,7 @@ void Slicer::convertToTsFromStream(const char* input, const char* output)
 
     const char * const vlc_args[] =
     {
-        "--verbose=2",
+        //"--verbose=2",
         param,
         //"--sout=#standard{access=file,mux=ts,dst=\"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream.ts\"}",
         "vlc://quit",
@@ -174,37 +187,75 @@ void Slicer::convertToTsFromStream(const char* input, const char* output)
     //libvlc_media_player_stop(mp);
 }
 
-void Slicer::makeMultipleSlices(const char* input, int number)
+void Slicer::makeMultipleSlices(const char* input, const char* output_location, int number)
 {
-    char output[1000];
-    int duration_of_slice = 30000; // in milliseconds
+    char tmp_output[1000];
+    char final_output[1000];
+    char tmp[100];
+    //int duration_of_slice = 20000; // in milliseconds //currently only holding the thread
+    int duration_of_slice = 20; //in sec
     for (long long int i=0;i<number;i++)
     {
-        sprintf(output
-                ,"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream_%lld"
+        sprintf(tmp_output
+                ,output_location //"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream_%lld"
+                //"\\tmp.ps"
+                //".ps"
+                //,i
+                );
+        sprintf(tmp
+                ,"\\tmp_%lld"  //"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream_%lld"
+                ".ps"
+                ,i
+                );
+        strcat(tmp_output,tmp); //"\\tmp.ps");
+        qDebug()<<tmp_output;
+        this->makeSliceFromStreamDirty(input,tmp_output,duration_of_slice);
+
+        sprintf(final_output
+                ,output_location);
+
+        sprintf(tmp
+                ,"\\Slice_%lld"  //"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream_%lld"
                 ".ts"
                 ,i
                 );
-        //qDebug()<<output;
-        this->makeSliceFromStreamDirty(input,output,duration_of_slice);
-        qDebug()<<"Duration of"<<output<<"is"<<getDuration(output);
+        strcat(final_output,tmp);
+        qDebug()<<final_output;
+        std::thread t1;
+        t1 = std::thread(convertToTS,this,tmp_output,final_output);
+        t1.detach();
+        //this->convertToTS(tmp_output,final_output);
+        qDebug()<<"Duration of"<<final_output<<"is"<<getDuration(tmp_output); //currently not working with stream
+        qDebug()<<"Slice made"<<i;
     }
 }
 
 void Slicer::makeSliceFromStreamDirty(const char *input, const char *output, int duration)
 {
-    const char* sout="--sout=#standard{access=file,mux=ts,dst=\"";
-    const char* ending="\"}";
+    //const char* sout="--sout=#standard{access=file,mux=ts,dst=\"";
+    const char* sout="--sout=#file{mux=ps,dst=";
+    const char* ending="}";
     char *param;
     param = new char[strlen(sout)+strlen(output)+strlen(ending)];
     strcat(strcpy(param,sout),output);
     strcat(param,ending);
 
+    std::string stop_time = "--run-time=";
+    stop_time+=std::to_string(duration);
+    char *param2;
+    param2 = new char[stop_time.length()];
+    strcpy(param2,stop_time.data());
+
+    qDebug()<<param2;
+
     const char * const vlc_args[] =
     {
         //"--verbose=2",
         param,
-        //"--run-time=5",
+        param2,
+        //"--live-caching",
+        //"--network-caching",
+        //"--run-time=10", //run time stared working well, so should use this instead
         //"--sout=#standard{access=file,mux=ts,dst=\"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream.ts\"}",
         "vlc://quit",
     };
@@ -215,10 +266,50 @@ void Slicer::makeSliceFromStreamDirty(const char *input, const char *output, int
 
     //QTest::qSleep(10000);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(duration));
+    std::this_thread::sleep_for(std::chrono::milliseconds((duration)*1000)); //TODO: Event catcher! or there might be some problems with threads
     qDebug()<<"Hi";
 
     libvlc_media_player_stop(mp);
+    //libvlc_release(inst);
+}
+
+
+void Slicer::testFunc()
+{
+    /*const char * const vlc_args[] =
+    {
+        "--verbose=2",
+        //"--live-caching=2000",
+        //"--network-caching",
+        //"--run-time=5",
+        //"--sout-mux-caching=100",
+        //"--network-caching=4000",
+        //"--sout-ts-dts-delay=30000",
+        //"--ts-seek-percent",
+        //"--sout=#standard{access=file,mux=ts,dst=\"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream.ts\"}",
+        "rtsp://ewns-hls-b-stream.hexaglobe.net/rtpeuronewslive/en_vidan750_rtp.sdp",
+        "--sout=#file{mux=mp4,dst=\"D:\\example\\outputFromStream.mp4\"}",
+        //"--sout=#transcode{}:duplicate{dst=std{access=file,mux=ts,dst=\"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream.ts\"}}",
+        //":sout-keep"
+        "--run-time=30",
+        //"--stop-time=10",
+        "vlc://quit",
+    };*/
+    const char * const vlc_args[] =
+    {
+        "--verbose=2",
+        //"--live-caching=4000",
+        "--sout=#file{mux=ps,dst=D:\\example\\outputFromStream.ts}",
+        "--run-time=10",
+        "vlc://quit"
+    };
+
+    inst=libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
+    m = libvlc_media_new_location(inst,"rtsp://ewns-hls-b-stream.hexaglobe.net/rtpeuronewslive/en_vidan750_rtp.sdp");  //"rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4");//"http://ar.solumedia.com.ar:1935/cool/hd/playlist.m3u8"
+    mp = libvlc_media_player_new_from_media (m);
+    //mp = libvlc_media_player_new(inst);
+    libvlc_media_player_play(mp);
+
 }
 
 
@@ -233,6 +324,7 @@ void cbVideoPrerender(void *p_video_data, uint8_t **pp_pixel_buffer, int size) {
 }
 void cbVideoPostrender(void *p_video_data, uint8_t *p_pixel_buffer
       , int width, int height, int pixel_pitch, int size, int64_t pts) {
+    qDebug()<<(dec)<<"Postrender"<<size<<pts<<p_video_data;
    // Unlocking
    imageMutex.unlock();
 }
@@ -241,7 +333,7 @@ void Slicer::makeSliceFromStreamSmem()
 {
     char smem_options[1000];
        sprintf(smem_options
-          , "#smem{"
+          , "#transcode{vcodec=RV24,acodec=s16l}:smem{"
              "video-prerender-callback=%lld,"
              "video-postrender-callback=%lld}"
           , (long long int)(intptr_t)(void*)&cbVideoPrerender
@@ -250,7 +342,7 @@ void Slicer::makeSliceFromStreamSmem()
           );
        //strcat(smem_options,"D:\\Work\\TD\\test\\Projects\\video\\example1.flv}");
 
-       strcat(smem_options,":std{access=file,mux=ts,dst=\"D:\\Work\\TD\\test\\Projects\\video\\outputFromOutput.ts\"}");
+       strcat(smem_options,":std{access=file,mux=ts,dst=\"D:\\Work\\TD\\test\\Projects\\video\\outputFromStream.ts\"}");
 
 
 
@@ -262,13 +354,17 @@ void Slicer::makeSliceFromStreamSmem()
         "--ignore-config", // Don't use VLC's config
         //"--extraintf=logger", // Log anything
         "--verbose=2", // Be verbose
-        "--sout", smem_options // Stream to memory
+        "--sout", smem_options, // Stream to memory
+        "--run-time=30",
+        "--stop-time=30",
+        "vlc://quit",
     };
     inst=libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
 
     //mp = libvlc_media_player_new(inst);
 
-    m = libvlc_media_new_path(inst,"D:\\Work\\TD\\test\\Projects\\video\\example1.flv");  //"http://ar.solumedia.com.ar:1935/cool/hd/playlist.m3u8"
+    //m = libvlc_media_new_path(inst,//"D:\\Work\\TD\\test\\Projects\\video\\example1.flv");  //"http://ar.solumedia.com.ar:1935/cool/hd/playlist.m3u8"
+    m = libvlc_media_new_location(inst,"rtsp://ewns-hls-b-stream.hexaglobe.net/rtpeuronewslive/en_vidan750_rtp.sdp");  //"rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4");
     mp = libvlc_media_player_new_from_media (m);
     libvlc_media_player_play(mp);
 }
