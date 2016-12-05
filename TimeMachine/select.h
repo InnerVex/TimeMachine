10acaab6f106326c804ce05f099251dc3c3c2eb7
InnerVex <mutalisk22@mail.ru>
2016-12-05 14:26:29 +0700

    Перенёс selectNextDateTime и selectDateTime в public

--- TimeMachine/select.h
+++ TimeMachine/select.h
@@ -15,11 +15,11 @@ public:
     static qint32  selectOffset          (quint32 sDateTime);
     static float   selectPercentOffset   (quint32 sDateTime);
     static QString selectNextFile        (QString fileName);
+    static qint32  selectNextDateTime   (QString fileName);
+    static qint32  selectDateTime       (QString fileName);
 
 
 private:
-    static qint32  selectNextDateTime   (QString fileName);
-    static qint32  selectDateTime       (QString fileName);
     static qint32  selectFileId         (QString fileName);
     static qint32  selectPathId         (quint32 sDateTime);
     static qint32  selectSourceId       (quint32 sDateTime);
